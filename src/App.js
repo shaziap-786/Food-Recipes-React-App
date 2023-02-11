7ec04ea90381f9fa29f37990e152b673af2955b4
Family <maheenpunjwani@Maheens-Air.lan>
2023-02-11 14:19:03 -0500

    food-recipes-react-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Axios from "axios"; // for recipies API
+import { v4 as uuidv4 } from "uuid"; // for recipies API
+import Recipe from "./components/Recipe";
+import Alert from "./components/Alert";
 
 function App() {
+  const [query, setQuery] = useState(""); //hook...used for tracking any state in application
+  const [recipes, setRecipes] = useState([]);
+  const [alert, setAlert] = useState("");
+
+  const APP_ID = "4e9f05eb";
+  const APP_KEY = "9b904d703fa0d46a88ce1ac63f29f498";
+
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  const getData = async () => {
+    if (query !== "") { //query ,example: tofu,cheese,chicken,etc
+      const result = await Axios.get(url);
+      if (!result.data.more) {
+        return setAlert("No food with such name");
+      }
+      console.log(result);
+      setRecipes(result.data.hits); //show all results
+      setQuery("");
+      setAlert("");
+    } else {
+      setAlert("Please fill the form");
+    }
+  };
+
+  const onChange = e => setQuery(e.target.value); //will get called when you type in search box 
+
+  const onSubmit = e => { //gets called when you hit go button
+
+    e.preventDefault();
+    getData();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Food Searching App</h1>
+      <form onSubmit={onSubmit} className="search-form">
+        {alert !== "" && <Alert alert={alert} />}
+        <input
+          type="text"
+          name="query"
+          onChange={onChange}
+          value={query}
+          autoComplete="off"
+          placeholder="Search Food"
+        />
+        <input type="submit" value="Search" />
+      </form>
+      <div className="recipes">
+        {recipes !== [] &&
+          recipes.map(recipe => <Recipe key={uuidv4()} recipe={recipe} />)} /
+      </div>
     </div>
   );
 }
